97dbcc252aabb49b3de0827eb84725c147b1a33f
YaaMe <969519545@qq.com>
2017-12-12 13:48:26 +0800

    feature(bitcoin/generate): add netcode input

--- bitcoin/generate.py
+++ bitcoin/generate.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 from pycoin.key import Key, BIP32Node
-
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--netcode", type=str, required=True, help='netcode: BTC/LTC/XTN/...')
+args = parser.parse_args()
 import os
 private_key = os.getenv('PK', os.urandom(32).encode('hex'))
 
@@ -8,7 +11,7 @@ print '===private_key==='
 print private_key
 the_key = Key(secret_exponent=int(private_key, 16))
 
-master_key = BIP32Node.BIP32Node.from_master_secret(bytes(the_key.wif().encode('ascii')), 'BTC')
+master_key = BIP32Node.BIP32Node.from_master_secret(bytes(the_key.wif().encode('ascii')), args.netcode)
 
 the_address = master_key.address()
 print '===withdraw address==='
